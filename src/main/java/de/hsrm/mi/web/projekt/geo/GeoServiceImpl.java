ca87afb990e3e381b98fbb64fb064550c186318a
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-05-20 23:16:12 +0200

    findeAdressInfo

--- src/main/java/de/hsrm/mi/web/projekt/geo/GeoServiceImpl.java
+++ src/main/java/de/hsrm/mi/web/projekt/geo/GeoServiceImpl.java
@@ -1,5 +1,6 @@
 package de.hsrm.mi.web.projekt.geo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -25,7 +26,16 @@ public class  GeoServiceImpl implements GeoService {
     
    
     WebClient web_Client = WebClient.create("https://nominatim.openstreetmap.org");
-        List<AdressInfo> antworten = web_Client.get()
+
+    List<AdressInfo> antworten = new ArrayList<>();
+
+    if(adresse == null || adresse.equals("")){
+        
+        return antworten;
+    
+    }else{
+        
+    antworten = web_Client.get()
                             .uri(UriBuilder -> UriBuilder
                             .path("/search")
                             .queryParam("q",adresse)
@@ -38,12 +48,25 @@ public class  GeoServiceImpl implements GeoService {
                         .block();                           
         
         
-        return null;
+                       
+        
+        return antworten;
+    
+        }
     }
 
     @Override
     public double abstandInKm(AdressInfo adr1, AdressInfo adr2) {
-        return 0;
+        
+        final double ERDRADIUS_KM = 6370;
+        var grad2rad = Math.PI / 180.0;
+        var phi_a = adr1.lat() * grad2rad;
+        var lambda_a = adr1.lon() * grad2rad;
+        var phi_b = adr2.lat() * grad2rad;
+        var lambda_b = adr2.lon() * grad2rad;
+        var zeta = Math.acos(Math.sin(phi_a) * Math.sin(phi_b) + Math.cos(phi_a)*Math.cos(phi_b)*Math.cos(lambda_b-lambda_a));
+        
+        return zeta * ERDRADIUS_KM;
     }
 
     @Override
