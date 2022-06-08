402b3610940227f22002bc98837c5f98380db9b2
Yannick Messinger <=>
2022-06-08 16:37:30 +0200

    geolink

--- frontend/src/components/GeoLink.vue
+++ frontend/src/components/GeoLink.vue
@@ -1,5 +1,7 @@
 <template>
     <h1>!!GEOLINK!!</h1>
+    <RouterLink to="/ort/ulm">Ulm schnell</RouterLink>
+
 </template>
 
 
@@ -9,6 +11,7 @@ defineProps<{ lat: number, lon: number, zoom?: number}>(),
 { zoom: 17 }
 )
 
+let testLink = "https://www.openstreetmap.org/#map=zoom/lat/lon"
 
 
 </script>
