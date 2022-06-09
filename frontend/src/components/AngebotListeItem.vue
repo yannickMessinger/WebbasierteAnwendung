4647f1df1789045e82f3d6d619e534afccd4f9c3
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-06-09 08:54:20 +0200

    AngebotList edit

---
+++ frontend/src/components/AngebotListeItem.vue
@@ -0,0 +1,17 @@
+<template>
+   
+   <tr>
+        <td >{{ beschreibung }}</td>
+        <td>{{ gebote}}</td>
+        <td>{{ topgebot }}</td>
+    
+    </tr>
+
+</template>
+
+<!-- oder hier wie slot nutzen um Textfelder anzuzeigen???-->
+<script  setup lang="ts">
+    let beschreibung = "Beschreibung-Dummy"
+    let gebote ="Gebote-Dummy"
+    let topgebot = "Topgebot-Dummy"
+</script>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="about">
+  
+  <div>
     <h1>This is an about page</h1>
+    <br/>
+    <GeoLink :lat="50.164802" :lon="8.624993" :zoom="18"></GeoLink>
+    <br/>
+    <AngebotListeItem></AngebotListeItem>
   </div>
+ 
 </template>
+
+<script  setup lang="ts">
+import GeoLink from '@/components/GeoLink.vue'
+import AngebotListeItem from '../components/AngebotListeItem.vue'
+</script>
 
 <style>
 @media (min-width: 1024px) {
