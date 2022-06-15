265f0766522f80ce61a8dcc20e76658279ca5a12
Yannick Messinger <=>
2022-06-15 15:52:46 +0200

    defineConfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,27 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/'
+      },
+      '/stompbroker': {
+        target: 'http://localhost:8080/',
+        ws: true
+      }
+    },
+  },
+
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+
+
+
+
   }
 })
