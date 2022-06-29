b4edb3f77fb7acd04c6164b3cb251e8c2d56f1ce
Yannick <yannick.messinger@student.hs-rm.de>
2022-06-29 11:33:07 +0200

    message properties angepasst

--- src/main/java/de/hsrm/mi/web/projekt/projektuser/ProjektUserServiceImpl.java
+++ src/main/java/de/hsrm/mi/web/projekt/projektuser/ProjektUserServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-import org.hibernate.TransientPropertyValueException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
